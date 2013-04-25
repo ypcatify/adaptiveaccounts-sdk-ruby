6e03032aca290e61b516603b408de699f15b4a54
siddick <mebramsha@paypal.com>
2013-04-25 15:00:29 +0530

    Added ipn_valid? method to validate IPN message

--- spec/adaptive_accounts_spec.rb
+++ spec/adaptive_accounts_spec.rb
@@ -3,16 +3,20 @@ require 'spec_helper'
 describe "AdaptiveAccounts" do
 
   before :all do
-    @client = PayPal::SDK::AdaptiveAccounts::API.new
+    @api= PayPal::SDK::AdaptiveAccounts::API.new
   end
 
   describe "Services" do
     PayPal::SDK::AdaptiveAccounts::Services.instance_methods.select{|m| m =~ /^[A-Z]/ and m !~ /^Build/ }.each do |service_method|
       it "make empty request to #{service_method}" do
-        response = @client.send(service_method, {})
+        response = @api.send(service_method, {})
         response.response_envelope.ack.should_not be_nil
       end
     end
+  end
+
+  it "Validate ipn message" do
+    @api.ipn_valid?("Invalid").should be_false
   end
 
 end
